23f9c3143e47be60c5eac9985d7da3b52282ff3a
Drak-Team <nafeesanjum2741495@gmail.com>
2026-03-07 20:37:17 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/', // یہ لائن شامل کی گئی ہے تاکہ پاتھ کا مسئلہ نہ ہو
-  server: {
-    host: "0.0.0.0", // یہاں تبدیلی کی گئی ہے
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: 'dist',
+  }
+});
